dbd63591583a128dd093556bda5163b245d99408
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2019-11-26 22:10:13 +0530

    superior: Enable Ccache
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- script_build.sh
+++ script_build.sh
@@ -2,14 +2,21 @@
 
 # This Script needs one change from the users and have some instructions how to use it, so please do read first 10 Lines of the Script.
 
+# Clone this script in your ROM Repo using following commands.
+# cd rom_repo
+# curl https://raw.githubusercontent.com/LegacyServer/Scripts/master/script_build.sh > script_build.sh
+
+# Some User's Details. Please fill it with your own details.
+
 # Replace "legacy" with your own SSH Username in lowercase
 username=sipun
 
 # Assign values to parameters used in Script from Jenkins Job parameters
-make_clean="$1"
-lunch_command="$2"
-device="$3"
-target_command="$4"
+use_ccache="$1"
+make_clean="$2"
+lunch_command="$3"
+device="$4"
+target_command="$5"
 
 # Colors makes things beautiful
 export TERM=xterm
@@ -20,11 +27,33 @@ export TERM=xterm
     cya=$(tput setaf 6)             #  cyan
     txtrst=$(tput sgr0)             #  Reset
 
+# CCACHE UMMM!!! Cooks my builds fast
+
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+export CCACHE_DIR=/home/subins/ccache/superior
+ccache -M 50G
+fi
+
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_EXEC=$(which ccache)
+export CCACHE_DIR=/home/subins/ccache/superior
+ccache -C
+export USE_CCACHE=1
+ccache -M 50G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
 
 # Its Clean Time
 if [ "$make_clean" = "yes" ];
 then
 rm -rf out/target/product/*
+#make Clean
 wait
 echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
 fi
